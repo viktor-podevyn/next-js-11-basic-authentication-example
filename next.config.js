116414f47bf3145a55954cc0ebbbc065b587525a
viktorpodevyn <viktor@sneakersgames.com>
2022-02-08 14:50:27 +0100

    api change

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reactStrictMode: true,
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
-            ? 'https://next-js-11-basic-authentication-example.vercel.app/' // development api
-            : 'https://next-js-11-basic-authentication-example.vercel.app/' // production api
+            ? 'https://next-js-11-basic-authentication-example.vercel.app/api/auth/' // development api
+            : 'https://next-js-11-basic-authentication-example.vercel.app/api/auth/' // production api
     }
 }
