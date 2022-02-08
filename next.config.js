28c1ff820239d025e5fcfc3a1e02fe6f41824ccf
viktorpodevyn <viktor@sneakersgames.com>
2022-02-08 14:49:22 +0100

    zgtrf

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
     reactStrictMode: true,
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            ? 'https://next-js-11-basic-authentication-example.vercel.app/' // development api
+            : 'https://next-js-11-basic-authentication-example.vercel.app/' // production api
     }
 }
